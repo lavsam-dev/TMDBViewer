123c2b54ff713c52bab685197767ec2b92a1a755
lavsam-dev <lavsam@gmail.com>
2021-08-02 17:25:57 +0400

    mainfragment

---
+++ app/src/main/java/com/geekbrains/lavsam/TMDBViewer/view/MainFragment.kt
@@ -0,0 +1,110 @@
+package com.geekbrains.lavsam.TMDBViewer.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.geekbrains.lavsam.TMDBViewer.R
+import com.geekbrains.lavsam.TMDBViewer.databinding.MainFragmentBinding
+import com.geekbrains.lavsam.TMDBViewer.model.AppState
+import com.geekbrains.lavsam.TMDBViewer.model.data.MovieDetail
+import com.geekbrains.lavsam.TMDBViewer.viewmodel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+    private val adapter = MainFragmentAdapter()
+    private var isDataSetLocal: Boolean = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+        adapter.removeOnItemViewClickListener()
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        adapter.setOnItemViewClickListener(object: OnItemViewClickListener {
+            override fun onItemViewClick(movieDetail: MovieDetail) {
+                val manager = activity?.supportFragmentManager
+                if (manager != null) {
+                    val bundle = Bundle()
+                    bundle.putParcelable(DetailsFragment.BUNDLE_EXTRA, movieDetail)
+                    manager.beginTransaction()
+                        .add(R.id.container, DetailsFragment.newInstance(bundle))
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+            }
+        })
+
+        binding.mainFragmentRecyclerView.adapter = adapter
+        binding.mainFragmentFAB.setOnClickListener {
+            changeMovieDataSet()
+        }
+        val observer = Observer<AppState> { a ->
+            renderData(a)
+        }
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+        viewModel.getMovieFromLocalSourse()
+    }
+
+    private fun changeMovieDataSet() {
+        if (isDataSetLocal) {
+            viewModel.getMovieFromLocalSourse()
+            binding.mainFragmentFAB.setImageResource(R.drawable.ic_world_movies)
+        } else {
+            viewModel.getMovieFromTMDBSource()
+            binding.mainFragmentFAB.setImageResource(R.drawable.ic_local_movies)
+        }
+        isDataSetLocal = !isDataSetLocal
+    }
+
+    private fun renderData(data: AppState) {
+        when (data) {
+            is AppState.Success -> {
+                val movieDetailData = data.movieData
+                binding.loadingLayout.visibility = View.GONE
+                adapter.setMovieDetail(movieDetailData)
+            }
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(binding.mainFragmentFAB, "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") {
+                        if (isDataSetLocal) viewModel.getMovieFromLocalSourse()
+                        else viewModel.getMovieFromTMDBSource()
+                    }
+                    .show()
+            }
+        }
+    }
+
+    interface OnItemViewClickListener {
+        fun onItemViewClick(movieDetail: MovieDetail)
+    }
+}
---
+++ app/src/main/java/com/geekbrains/lavsam/TMDBViewer/view/MainFragmentAdapter.kt
@@ -0,0 +1,54 @@
+package com.geekbrains.lavsam.TMDBViewer.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.geekbrains.lavsam.TMDBViewer.model.data.MovieDetail
+
+class MainFragmentAdapter :
+    RecyclerView.Adapter<MainFragmentAdapter.MainViewHolder>() {
+
+        private var movieDetailData: List<MovieDetail> = listOf()
+        private var onItemViewClickListener: MainFragment.OnItemViewClickListener? = null
+
+        fun setOnItemViewClickListener(onItemViewClickListener: MainFragment.OnItemViewClickListener){
+            this.onItemViewClickListener = onItemViewClickListener
+        }
+
+        fun removeOnItemViewClickListener(){
+            onItemViewClickListener = null
+        }
+
+        fun setMovieDetail(data: List<MovieDetail>) {
+            movieDetailData = data
+            notifyDataSetChanged()
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val binding = MainRecyclerItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+            return MainViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            holder.bind(movieDetailData[position])
+        }
+
+        override fun getItemCount(): Int {
+            return movieDetailData.size
+        }
+
+        inner class MainViewHolder(val binding: MainRecyclerItemBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(movieDetail: MovieDetail) {
+                binding.mainFragmentRecyclerItemTextView.text = movieDetail.movie.name
+                binding.root.setOnClickListener {
+                    onItemViewClickListener?.onItemViewClick(movieDetail)
+                }
+            }
+        }
+}
