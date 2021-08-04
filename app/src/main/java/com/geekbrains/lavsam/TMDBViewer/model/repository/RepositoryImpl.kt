f7b40327b81a6833b420ffad7ae1271fc030f5b5
lavsam-dev <lavsam@gmail.com>
2021-08-04 21:18:02 +0400

    исправлена ошибка при входе в детализацию.

--- app/src/main/java/com/geekbrains/lavsam/TMDBViewer/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/geekbrains/lavsam/TMDBViewer/model/repository/RepositoryImpl.kt
@@ -5,7 +5,8 @@ import com.geekbrains.lavsam.TMDBViewer.model.data.getMovies
 
 class RepositoryImpl : Repository {
     override fun getMovieFromTMDB(): List<MovieDetail> {
-        return listOf(MovieDetail())
+//        return listOf(MovieDetail())
+        return getMovies()
     }
 
     override fun getMovieFromLocalStorage(): List<MovieDetail> {
