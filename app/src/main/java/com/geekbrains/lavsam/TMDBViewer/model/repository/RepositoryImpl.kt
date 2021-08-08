460f5996ace3170724558981b42e46cba6ef6640
lavsam-dev <lavsam@gmail.com>
2021-08-08 19:05:42 +0400

    рефакторинг RepositoryImpl.kt

--- app/src/main/java/com/geekbrains/lavsam/TMDBViewer/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/geekbrains/lavsam/TMDBViewer/model/repository/RepositoryImpl.kt
@@ -4,11 +4,8 @@ import com.geekbrains.lavsam.TMDBViewer.model.data.MovieDetail
 import com.geekbrains.lavsam.TMDBViewer.model.data.getMovies
 
 class RepositoryImpl : Repository {
-    override fun getMovieFromTMDB(): List<MovieDetail> {
-        return getMovies()
-    }
 
-    override fun getMovieFromLocalStorage(): List<MovieDetail> {
-        return getMovies()
-    }
+    override fun getMovieFromTMDB(): List<MovieDetail> = getMovies()
+
+    override fun getMovieFromLocalStorage(): List<MovieDetail> = getMovies()
 }
